6513ab9f368ded3425d5f06be26becc61400b992
Sanskar Khandelwal <sanskarkhandelwal336@gmail.com>
2025-03-04 14:46:12 +0530

    Simple Dictionary

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import pandas as pd
 
 app = Flask(__name__)
 
+df = pd.read_csv("dictionary.csv")
 
 def find_word(name):
-    df = pd.read_csv("dictionary.csv")
-    defination = (df.loc[df['word'] == name], 'definition')
-    if not definition.empty:
-        return definition.squeeze()  # Return the definition
+    definition = df.loc[df['word'].str.lower() == name.lower(), 'definition']
+    if not definition.empty:  # Check if the result is not empty
+        return definition.iloc[0]  # Return the first (and only) definition
     else:
         return None  
-    return defination
 
+
+@app.route("/")
+def home_page():
+    return render_template("index.html")
 
 @app.route("/api/v1/<word>")
 def hello_world(word):
-    defination = find_word(word)
-    if defination:       return jsonify({
+    definition = find_word(word)
+    if definition:
+            return jsonify({
             "word": word,
-            "definition": defination
+            "definition": definition
         })
     else:
         return jsonify({
